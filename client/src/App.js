0b3fe41189bc0fa6417cabf87f15d3d919045c14
Besufekad-HAZ <besurye@gmail.com>
2023-12-11 13:44:39 +0300

    Setup store providers and modes for themes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import createTheme from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
 
 function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return <div className="app">
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
+  </div>;
 }
 
 export default App;
