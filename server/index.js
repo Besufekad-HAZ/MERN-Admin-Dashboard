a1752f089cbba3e49722c08c6774a97df44fe4a5
Besufekad-HAZ <besurye@gmail.com>
2023-12-11 11:33:35 +0300

    Configure and connect mongoose database and setup the port to connect

--- server/index.js
+++ server/index.js
@@ -29,3 +29,15 @@ app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(`${error} did not connect`);
+  });
