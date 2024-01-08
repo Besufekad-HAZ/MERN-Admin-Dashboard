8193514248493d6a68d3ebb39992906d489c7369
Besufekad-HAZ <besurye@gmail.com>
2024-01-08 13:29:48 +0300

    Configure the RTK api in state/api of the front end for fetching transactions

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -33,8 +33,20 @@ export const api = createApi({
       query: () => "client/customers",
       providesTags: ["Customers"],
     }),
+    getTransactions: build.query({
+      query: ({ page, pageSize, sort, search }) => ({
+        url: "client/transactions",
+        method: "GET",
+        params: { page, pageSize, sort, search },
+      }),
+      providesTags: ["Transactions"],
+    }),
   }),
 });
 
-export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery } =
-  api;
+export const {
+  useGetUserQuery,
+  useGetProductsQuery,
+  useGetCustomersQuery,
+  useGetTransactionsQuery,
+} = api;
