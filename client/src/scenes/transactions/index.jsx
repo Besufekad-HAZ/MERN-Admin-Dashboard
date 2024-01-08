8b3207a27ad0fb019f855ab44c2293f77cbdc28c
Besufekad-HAZ <besurye@gmail.com>
2024-01-08 14:04:52 +0300

    Add transactions array to display

--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -19,7 +19,38 @@ const Transactions = () => {
     sort: JSON.stringify(sort),
     search,
   });
-  console.log("data", data);
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "CreatedAt",
+      flex: 1,
+    },
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
+
   return <div>Transactions!!</div>;
 };
 
