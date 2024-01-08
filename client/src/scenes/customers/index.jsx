72e11b91f5fea5505770d46f9bdc20f189fbcf8e
Besufekad-HAZ <besurye@gmail.com>
2024-01-08 11:05:23 +0300

    Setup the customer page on the front end

---
+++ client/src/scenes/customers/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { useGetCustomersQuery } from "state/api";
+import Header from "components/Header";
+const index = () => {
+  return <div></div>;
+};
+
+export default index;
