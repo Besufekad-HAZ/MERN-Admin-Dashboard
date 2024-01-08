f7b7b835b4457a887582a02c4466dc8e38b2e2fa
Besufekad-HAZ <besurye@gmail.com>
2024-01-08 14:11:24 +0300

    Style the basic transactions layout before implementing server side pagination

--- client/src/scenes/transactions/index.jsx
+++ client/src/scenes/transactions/index.jsx
@@ -51,7 +51,45 @@ const Transactions = () => {
     },
   ];
 
-  return <div>Transactions!!</div>;
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="TRANSACTIONS" subtitle="Entire list of transactions." />
+      <Box
+        height="80vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={(data && data.transactions) || []}
+          columns={columns}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Transactions;
