cd1886c2ca6a34927deaadc01ee64d5e02f1ec5f
Besufekad-HAZ <besurye@gmail.com>
2024-01-08 12:04:28 +0300

    Finish displaying list of customers usding MUI datagrid and other table features

--- client/src/scenes/customers/index.jsx
+++ client/src/scenes/customers/index.jsx
@@ -7,7 +7,61 @@ const Customers = () => {
   const theme = useTheme();
   const { data, isLoading } = useGetCustomersQuery();
   console.log("data", data);
-  return <div>Customers</div>;
+
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 0.5,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params) => {
+        return params.value.replace(/^(\d{3})(\d{3})(\d{4})$/, "($1)$2-$3");
+      },
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      flex: 0.4,
+    },
+    {
+      field: "occupation",
+      headerName: "Occupation",
+      flex: 1,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      flex: 0.5,
+    },
+  ];
+
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="CUSTOMERS" subtitle="List of Customers" />
+      <Box mt="40px" height="75vh">
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={data || []}
+          columns={columns}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default Customers;
