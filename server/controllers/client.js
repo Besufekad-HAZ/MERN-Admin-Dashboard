9ccfcb2819c0c4c775de451c39e5f4f015a341ef
Besufekad-HAZ <besurye@gmail.com>
2024-01-21 22:01:52 +0300

    Update the getGeography controllers to fetch and convert country formats

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,6 +2,7 @@ import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
+import getCountryIso3 from "country-iso-2-to-3";
 
 export const getProducts = async (req, res) => {
   try {
@@ -70,12 +71,27 @@ export const getTransactions = async (req, res) => {
   }
 };
 
-
 export const getGeopraphy = async (req, res) => {
   try {
     const users = await User.find({ role: "user" });
 
-    const mappedLocations = users.reduce()
+    const mappedLocations = users.reduce((acc, { country }) => {
+      const countryISO3 = getCountryIso3(country);
+      if (!acc[countryISO3]) {
+        acc[countryISO3] = 0;
+      }
+      acc[countryISO3]++;
+      return acc;
+    }, {});
 
+    const formattedLocations = Object.entries(mappedLocations).map(
+      ([country, count]) => {
+        return { id: country, value: count };
+      }
+    );
+
+    res.status(200).json(formattedLocations);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
   }
-}
+};
