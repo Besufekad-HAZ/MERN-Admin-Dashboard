e6c6bfe939a8431b03d886de71e690dc299bcb20
Besufekad-HAZ <besurye@gmail.com>
2024-01-22 15:00:43 +0300

    Implement the controllers for the admin page in the backend

---
+++ server/controllers/management.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import User from "../models/user";
+
+export const getUsers = async (req, res) => {
+  try {
+    const admins = await User.find({ role: "admin" }).select("-password");
+    res.status(200).json(admins);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
